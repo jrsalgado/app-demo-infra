4fc82a8428be3ea998dcf54afdb97b4059481ffc
Jayro Salgado <jsalgado@nearsoft.com>
2017-06-08 09:00:44 -0700

    change services code to app directory

---
+++ app/README.md
@@ -0,0 +1,3 @@
+# APP SOURCE
+- /api-demo
+- /web-app-demo
--- develop-env.sh
+++ develop-env.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "You need to intstall GIT and run DOCKER start"
+echo "You need to intstall GIT and run DOCKER"
 
 echo "Git clone api-demo on sibling directory"
 echo "please remove /api-demo directory if exists"
-git clone -b develop https://github.com/jrsalgado/api-demo.git ../api-demo
+git clone -b develop https://github.com/jrsalgado/api-demo.git ./app/api-demo
 
 echo "Git clone web-app-demo on sibling directory"
 echo "Please remove /web-app-demo directory if exists"
-git clone -b develop https://github.com/jrsalgado/api-demo.git ../web-app-demo
+git clone -b develop https://github.com/jrsalgado/web-app-demo.git ./app/web-app-demo
 
 echo "Build images from api-demo and web-app-demo"
 docker-compose -f docker-compose.yml -f develop.yml build --no-cache
